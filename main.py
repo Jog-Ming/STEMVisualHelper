89ba3c11ac579c48f24fe8c43df3526c89a60143
Jog_Ming <jog_ming@outlook.com>
2024-05-06 18:25:55 +0800

    - Basic pygame setup

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+
+
+pygame.init()
+info = pygame.display.Info()
+screen_width = info.current_w
+screen_height = info.current_h
+window_width = screen_width // 2
+window_height = screen_height // 2
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption('STEM Visual Helper')
+clock = pygame.time.Clock()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
